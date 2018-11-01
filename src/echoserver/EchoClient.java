fb63720841db86d7438a05a6f910cf267ce8e3cc
Mark Schauer <marksthe1@gmail.com>
2018-11-01 11:11:24 -0500

    Create broken thread code we will fix later

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -6,23 +6,44 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
+    public static final int PORT_NUMBER = 6013;
 
-	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
-	}
+    public static void main(String[] args) throws IOException {
+        EchoClient client = new EchoClient();
+        client.start();
+    }
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
-		int readByte;
-		while ((readByte = System.in.read()) != -1) {
-			socketOutputStream.write(readByte);
-			int socketByte = socketInputStream.read();
-			System.out.write(socketByte);
-		}
-		System.out.flush();
-	}
+    private void start() throws IOException {
+        Socket socket = new Socket("localhost", PORT_NUMBER);
+        InputStream socketInputStream = socket.getInputStream();
+        OutputStream socketOutputStream = socket.getOutputStream();
+        Thread t1 = new Thread(new keyboardReader());
+        Thread t2 = new Thread(new serverWriter());
+        t1.start();
+        t2.start();
+    }        System.out.flush();
+}
+
+public class keyboardReader implements Runnable {
+    public void run() {
+        try {
+            int readByte;
+            while((readByte = System.in.read()) != -1) {
+                socketOutputStream.write(readByte);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class serverWriter implements Runnable {
+    public void run() {
+        try {
+            int socketByte = socketInputStream.read();
+            System.out.write(socketByte);
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
