a069823294af70788276463d9d3edd49fa44243a
Mark Schauer <marksthe1@gmail.com>
2018-11-06 11:03:14 -0600

    Got client tests to pass, now for server!!!

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -18,27 +18,45 @@ public class EchoClient {
         InputStream socketInputStream = socket.getInputStream();
         OutputStream socketOutputStream = socket.getOutputStream();
 
-        Thread t1 = new Thread(new KeyboardReader(socketOutputStream));
-        Thread t2 = new Thread(new ServerWriter(socketInputStream));
+        Thread t1 = new Thread(new KeyboardReader(socketOutputStream, socket));
+        Thread t2 = new Thread(new ServerWriter(socketInputStream, socket));
+
         t1.start();
         t2.start();
 
+
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.flush();
     }
 }
 
  class KeyboardReader implements Runnable {
     private OutputStream oStream;
+    private Socket socket;
 
-    public KeyboardReader(OutputStream oStream) {
+    public KeyboardReader(OutputStream oStream, Socket socket) {
+        this.socket = socket;
         this.oStream = oStream;
     }
 
     public void run() {
         try {
             int readByte;
-            while((readByte = System.in.read()) != -1) {
+            while ((readByte = System.in.read()) != -1) {
                 oStream.write(readByte);
             }
+
+            socket.shutdownOutput();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -47,15 +65,21 @@ public class EchoClient {
 
  class ServerWriter implements Runnable {
     private InputStream iStream;
+    private Socket iSocket;
 
-    public ServerWriter(InputStream iStream) {
+    public ServerWriter(InputStream iStream, Socket iSocket) {
         this.iStream = iStream;
+        this.iSocket = iSocket;
     }
     public void run() {
         try {
-            int socketByte = iStream.read();
-            System.out.write(socketByte);
-            } catch (IOException e) {
+            int socketByte;
+            while ((socketByte = iStream.read()) != -1){
+                System.out.write(socketByte);
+            }
+
+            iSocket.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
